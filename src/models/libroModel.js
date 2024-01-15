2057bee3855914d81dffdbee62049f64f34edc2b
rolog26 <rodrigo.gigena2611@gmail.com>
2024-01-15 15:21:11 -0300

    Test Unitarios y de Integración

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_DB, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
 
-const LibroSchema = new mongoose.Schema({
-  titulo: String,
-  autor: String
-}, { collection: 'libros' });
+const LibroSchema = new mongoose.Schema(
+  {
+    titulo: String,
+    autor: String,
+  },
+  { collection: "libros" }
+);
 
-const Libro = mongoose.model('Libro', LibroSchema);
+const Libro = mongoose.model("Libro", LibroSchema);
 
 module.exports = Libro;
